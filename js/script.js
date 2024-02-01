3ee27c3945e20a411facb6453e078c48c4d421ee
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-02-01 23:28:49 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const darkModeToggle = document.getElementById('darkmode-toggle');
 const loader = document.querySelector(".loader");
 const menu_btn = document.querySelector('.hamburguer');
 const mobile_menu = document.querySelector('.nav-itens-mobile');
+const text = document.querySelector(".multiple-text");
 
 //Função Light/Dark
 function toggleDarkMode() {
@@ -23,8 +24,25 @@ function toggleMobileMenu() {
     mobile_menu.classList.toggle('is-active');
 }
 
+//Função animação texto inicial
+function textLoad() {
+    setTimeout(() => {
+      text.textContent = "Front-End Developer";
+    }, 0);
+    setTimeout(() => {
+      text.textContent = "Full-Stack Developer";
+    }, 4000);
+    setTimeout(() => {
+      text.textContent = "UX/UI Designer";
+    }, 8000);
+}
+
 darkModeToggle.addEventListener('change', toggleDarkMode);
 
-window.addEventListener("load", hideLoader);
+window.addEventListener("load", () => {
+    hideLoader();
+    textLoad(); // Inicia o ciclo de texto no carregamento da página
+    setInterval(textLoad, 12000);
+  });
 
 menu_btn.addEventListener('click', toggleMobileMenu);
